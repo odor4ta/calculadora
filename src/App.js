444d2339b79a4137a5025ac67ff1f6c66693a898
odor4ta <ned.yit18@gmail.com>
2022-07-01 23:07:23 -0500

    botones y estilos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import tatiLearnLogo from './imagenes/tatilearn-logo.png'
+import Boton from './componentes/Boton';
 
 function App() {
   return (
@@ -11,7 +12,10 @@ function App() {
           alt='Logo de tati learn' />
       </div>
       <div className='contenedor-calculadora'>
-        <div className='fila'></div>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+        </div>
         <div className='fila'></div>
         <div className='fila'></div>
         <div className='fila'></div>
