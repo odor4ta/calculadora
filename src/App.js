0d21feb6ebf774df3a8285ff5bd16b2b19307a0e
odor4ta <ned.yit18@gmail.com>
2022-07-01 22:03:44 -0500

    cuerpo y logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import tatiLearnLogo from './imagenes/tatilearn-logo.png'
 
 function App() {
   return (
-    <div className="App">
-      Hola
+    <div className='App'>
+      <div className='tatilearn-logo-contenedor'>
+        <img
+          src={tatiLearnLogo}
+          className='tatilearn-logo'
+          alt='Logo de tati learn' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
